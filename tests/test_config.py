d65666d3b79c822df1a5bd3064f62ee0105762bd
David Seddon <david@seddonym.me>
2017-05-22 09:51:49 +0100

    100% coverage

--- tests/test_config.py
+++ tests/test_config.py
@@ -67,23 +67,59 @@ class TestConfig(TestCase):
             }
             assert config.get_uncategorized_account_names() == [sentinel.foo_account, sentinel.bar_account]
 
-    def test_get_patterns_for_account_name(self):
+    def assert_get_patterns_for_account_name_returns_patterns(self, account_name, matches, patterns):
         with patch.object(Config, '_load_from_file'):
             config = Config(sentinel.filename)
             config._config_dict = {
-                'matches': [
-                    {'Another Account': [
-                        {'Foo:Bar': ['WRONG']},
-                    ]},
-                    {'Imbalance Account': [
-                        {'Foo:Bar': ['FOOBAZ', 'FOOBAR ?']},
-                        {'Baz': ['baz baz *']}
-                    ]},
-                ],
+                'matches': matches,
             }
-            expected_patterns = [
-                MatchPattern(pattern='FOOBAZ', account_name='Foo:Bar'),
-                MatchPattern(pattern='FOOBAR ?', account_name='Foo:Bar'),
-                MatchPattern(pattern='baz baz *', account_name='Baz'),
-            ]
-            assert config.get_patterns_for_account_name('Imbalance Account') == expected_patterns
+
+        assert config.get_patterns_for_account_name(account_name) == patterns
+
+    def test_get_patterns_for_account_name(self):
+        matches = [
+            {'Another Account': [
+                {'Foo:Bar': ['WRONG']},
+            ]},
+            {'Imbalance Account': [
+                {'Foo:Bar': ['FOOBAZ', 'FOOBAR ?']},
+                {'Baz': ['baz baz *']}
+            ]},
+        ]
+        expected_patterns = [
+            MatchPattern(pattern='FOOBAZ', account_name='Foo:Bar'),
+            MatchPattern(pattern='FOOBAR ?', account_name='Foo:Bar'),
+            MatchPattern(pattern='baz baz *', account_name='Baz'),
+        ]
+        self.assert_get_patterns_for_account_name_returns_patterns('Imbalance Account', matches, expected_patterns)
+
+    def test_get_patterns_for_account_name_returns_empty_list_for_account_not_in_config(self):
+        matches = [
+            {'Another Account': [
+                {'Foo:Bar': ['WRONG']},
+            ]},
+            {'Imbalance Account': [
+                {'Foo:Bar': ['FOOBAZ', 'FOOBAR ?']},
+                {'Baz': ['baz baz *']}
+            ]},
+        ]
+        self.assert_get_patterns_for_account_name_returns_patterns('Unlisted account', matches, [])
+
+    def assert_get_only_key_from_dictionary_raises_value_error(self, dictionary):
+        try:
+            Config._get_only_key_from_dictionary(dictionary)
+        except ValueError as e:
+            assert str(e) == '_get_only_key_from_dictionary received a ' \
+                'dictionary with {} keys.'.format(len(dictionary))
+        else:
+            assert False, '_get_only_key_from_dictionary did not raise ValueError.'
+
+    def test_get_only_key_from_empty_dictionary(self):
+        self.assert_get_only_key_from_dictionary_raises_value_error({})
+
+    def test_get_only_key_from_dictionary_with_multiple_keys(self):
+        self.assert_get_only_key_from_dictionary_raises_value_error({'foo': 1, 'bar': 2, 'baz': 3})
+
+    def test_get_only_key_from_dictionary_with_one_key(self):
+        result = Config._get_only_key_from_dictionary({'foo': 1})
+        assert result == 'foo'
